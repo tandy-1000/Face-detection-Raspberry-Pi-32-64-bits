dc638be15344cce88b0e6f95916a8fe4263379e7
tandy1000 <tandy1000@cock.li>
2022-10-29 22:13:47 +0100

    remove fps count

--- MNN/main.cpp
+++ MNN/main.cpp
@@ -5,18 +5,11 @@
 using namespace std;
 
 int main(int argc, char **argv) {
-    float f;
-    float FPS[16];
-    int i, Fcnt=0;
     cv::Mat frame;
-    //some timing
-    chrono::steady_clock::time_point Tbegin, Tend;
-
-    for(i=0;i<16;i++) FPS[i]=0.0;
 
     UltraFace ultraface("slim-320-quant-ADMM-50.mnn", 320, 240, 4, 0.65); // config model input
 
-    cv::VideoCapture cap("Walks2.mp4");
+    cv::VideoCapture cap(0);
     if (!cap.isOpened()) {
         cerr << "ERROR: Unable to open the camera" << endl;
         return 0;
@@ -31,12 +24,8 @@ int main(int argc, char **argv) {
             break;
         }
 
-        Tbegin = chrono::steady_clock::now();
-
         vector<FaceInfo> face_info;
         ultraface.detect(frame, face_info);
-
-        Tend = chrono::steady_clock::now();
 
         for (auto face : face_info) {
             cv::Point pt1(face.x1, face.y1);
@@ -44,16 +33,10 @@ int main(int argc, char **argv) {
             cv::rectangle(frame, pt1, pt2, cv::Scalar(0, 255, 0), 2);
         }
 
-        //calculate frame rate
-        f = chrono::duration_cast <chrono::milliseconds> (Tend - Tbegin).count();
-        if(f>0.0) FPS[((Fcnt++)&0x0F)]=1000.0/f;
-        for(f=0.0, i=0;i<16;i++){ f+=FPS[i]; }
-        cv::putText(frame, cv::format("FPS %0.2f", f/16),cv::Point(10,20),cv::FONT_HERSHEY_SIMPLEX,0.6, cv::Scalar(0, 0, 255));
-
-//        cv::imwrite("27Result.jpg",frame); //in case you run only a jpg picture
-
         //show output
-        cv::imshow("RPi 64 OS - 1,95 GHz - 2 Mb RAM", frame);
+        cv::imshow("Name", frame);
+        cv::namedWindow("Name", cv::WINDOW_NORMAL);
+        cv::setWindowProperty("Name", cv::WND_PROP_FULLSCREEN, cv::WINDOW_FULLSCREEN);
         char esc = cv::waitKey(5);
         if(esc == 27) break;
     }
